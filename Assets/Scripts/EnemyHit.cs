80663321338d12581db65893fe67b88a6e2362e1
M4k <M4kvrstudios@gmail.com>
2020-04-23 08:39:50 +0200

    initialCommit

---
+++ Assets/Scripts/EnemyHit.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour
+{
+    public bool test;
+    Rigidbody[] rigidbodies;
+    Collider[] colliders;
+
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        colliders = GetComponentsInChildren<Collider>();
+        rigidbodies = GetComponentsInChildren<Rigidbody>();
+        foreach (var rb in rigidbodies)
+        {
+            //Skip parent rb
+            if(rb.useGravity == true)
+            {
+                rb.isKinematic = true;
+                rb.mass = 100;
+            }
+        }
+
+  
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void KillEnemy()
+    {
+        animator.enabled = false;
+        foreach (var rb in rigidbodies)
+        {
+            rb.isKinematic = false;
+        }
+        foreach (var collider in colliders)
+        {
+            collider.enabled = false;
+        }
+        Invoke("DisableEnemy", 2f);
+    }
+
+    private void DisableEnemy()
+    {
+
+       gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    private float speed = 3f;
+    private Rigidbody rigidbody;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rigidbody = GetComponent<Rigidbody>();
+        transform.LookAt(player.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = transform.forward * speed;
+      rigidbody.velocity = new Vector3(movement.x, rigidbody.velocity.y, movement.z);
+    }
+}
